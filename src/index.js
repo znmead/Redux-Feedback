1e17533ea8ebe9976f7ec19060bf5ebc742b9dd2
Zach Mead <zachary.n.mead@gmail.com>
2021-02-05 16:46:06 -0600

    Added middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger'; // added for debuggin/visibility of state/payloads
 
 // TODO: Create four reducers
 
-const storeInstance = createStore(
+const storeInstance = createStore( // creates store to "stores" reducers
     combineReducers(
         {
 
@@ -19,8 +20,9 @@ const storeInstance = createStore(
             // Reducer 3
 
             // Reducer 4
-        }
-    )
+        }),
+    applyMiddleware(logger),
+
 );
 
 ReactDOM.render(
