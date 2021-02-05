b03b67979e63d7b460af6a1d989622f514c51b35
Zach Mead <zachary.n.mead@gmail.com>
2021-02-05 16:29:45 -0600

    All dependencies installed/index.js setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// TODO: Create four reducers
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+
+            // Reducer 1
+
+            // Reducer 2
+
+            // Reducer 3
+
+            // Reducer 4
+        }
+    )
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+
+
