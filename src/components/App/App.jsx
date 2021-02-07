b4b31f40fd17abd6057466ba378319a6e676741e
Zach Mead <zachary.n.mead@gmail.com>
2021-02-07 12:56:35 -0600

    Feeling route working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
-// import Admin from '../Admin/Admin';
-//TODO: import info
 import { useDispatch } from 'react-redux';
-
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+//TODO: import info
+
+import Feeling from '../Feeling/Feeling.jsx'; 
+// import Admin from '../Admin/Admin';
+// impor
+
 
 
 function App() {
@@ -28,40 +30,39 @@ function App() {
       })
   }
 
-  /* <Router>
-  <Header />
-
-  <div className='App container'>
-    <Switch>
-      <Route exact path='/' component={Home} getHome={getHome} />
-      <Route path='/feeling' component={Feeling} getFeeling={getFeeling} />
-      <Route path='/understanding' component={Understanding} getUnderstanding={getUnderstanding} />
-      <Route path='/support' component={Support} getSupport={getSupport} />
-      <Route path='/comments' component={Comments} getComments={getComments} />
-      <Route path='/review' component={Review} getReview={getReview} />
-
-      <Route path='/Admin' component={Admin} />
-      <Route path='/Admin' component={Admin} />
-
-    </Switch>
-  </div>
-  <Footer />
-</Router> */
-
-
-
-  /*  */
-
+  // TODO: Add <Header />
+  // TODO: Add <Footer />
+  // TODO: Add <Route exact path='/' component={Home} getHome={getHome} />
+ /* <Route path='/understanding' component={Understanding} getUnderstanding={getUnderstanding} />
+          <Route path='/support' component={Support} getSupport={getSupport} />
+          <Route path='/comments' component={Comments} getComments={getComments} />
+          <Route path='/review' component={Review} getReview={getReview} />
+          <Route path='/Admin' component={Admin} /> */ 
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-      
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
 
-    </div>
+
+      </div>
+
+
+      <div className='App container'>
+        <Switch>
+          
+          <Route path='/feeling' component={Feeling} getFeedback={getFeedback} />
+          
+    
+
+        </Switch>
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/components/Feeling/feeling.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom'; 
+import { useDispatch } from 'react-redux';
+
+// import { useSelector } from 'react-redux'; 
+
+
+function Feeling () {
+    // 
+
+
+}
+
+export default Feeling;
